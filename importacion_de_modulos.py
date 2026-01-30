5beaf0d293eebf9e582f5cc11fd4bac28df38305
andreslauria <andreslauria@gmail.com>
2026-01-30 00:25:39 -0300

    final

--- importacion_de_modulos.py
+++ importacion_de_modulos.py
@@ -2,12 +2,15 @@
 # Podemos importar módulos integrados de Python o crear nuestros propios módulos personalizados.
 
 
+from mi_paquete import modulo_desde_paquete
+
 import datetime
 import random
 # tmb podemos importar funciones específicas de un módulo utilizando la sintaxis from módulo import función.
 from math import exp
 import math
 import mi_modulo
+
 
 # Uso de funciones del módulo mi_modulo
 mi_modulo.saludar("Carlos")  # Imprime "¡Hola, Carlos!"
@@ -30,3 +33,7 @@ print(numero_aleatorio)  # Imprime un número entero aleatorio entre 1 y 10
 
 fecha_actual = datetime.datetime.now()
 print(fecha_actual)  # Imprime la fecha y hora actual
+
+
+# Imprime "Hola desde el módulo dentro del paquete!"
+modulo_desde_paquete.hola_mundo()
